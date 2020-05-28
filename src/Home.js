009da319471316877f7471d594f1d7c7a2d5946c
kennja05 <jacobkenny05@gmail.com>
2020-05-28 11:45:43 -0400

    updated homepage

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,14 @@ import React from 'react'
 const Home = () => {
     return(
         <div className='home container'>
-                <h1>
-                    <span className='jacob'>Jacob Kenny</span>
-                </h1>
-            
+                <div className='name'>
+                    <h1>
+                        <span className='jacob'>Jacob</span>
+                    </h1>
+                    <h1>
+                        <span className='jacob'>Kenny</span>
+                    </h1>
+                </div>
                 <h3 style={{fontFamily: 'Oswald'}}>
                     software developer
                 </h3>
