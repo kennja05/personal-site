f09634d10d0b81d89cf42a9fcb2fc17401392556
kennja05 <jacobkenny05@gmail.com>
2020-05-27 22:03:58 -0400

    updated title

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 const Home = () => {
     return(
-        <div className='home'>
-            <h1>
-                Jacob Kenny
-            </h1>
-            <h3 style={{fontFamily: 'Oswald'}}>
-                software developer
-            </h3>
+        <div className='home container'>
+                <h1>
+                    <span className='jacob'>Jacob Kenny</span>
+                </h1>
+            
+                <h3 style={{fontFamily: 'Oswald'}}>
+                    software developer
+                </h3>
         </div>
     )
 }
