a49f9d817dd1edb4146dc23e7c7ee84b57af6ae7
kennja05 <jacobkenny05@gmail.com>
2020-05-28 14:36:42 -0400

    updated spacing of projects page

--- src/Projects.js
+++ src/Projects.js
@@ -2,36 +2,55 @@ import React from 'react'
 
 const Projects = () => {
     return(
-        <div>
-            
-            <iframe 
-                width="560" height="315" src="https://www.youtube.com/embed/SgrQfCzyd_o" 
-                title='states-game-demo' frameborder="0" 
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-                allowfullscreen='true'>
-            </iframe>
+        <div className='projects'>
 
-            <iframe 
-                width="560" height="315" src="https://www.youtube.com/embed/BNmb7fpG_LQ" 
-                title='image-scrambler-game-demo' frameborder="0" 
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-                allowfullscreen='true'>
-            </iframe>
+            <div className='proj'>
 
-            <iframe 
-                width="560" height="315" src="https://www.youtube.com/embed/a4EL71uBcW8" 
-                title='trip-tracker-demo' frameborder="0" 
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-                allowfullscreen='true'>
-            </iframe>
+                <div>
+                    <iframe 
+                        className='media-object'
+                        width="560" height="315" src="https://www.youtube.com/embed/SgrQfCzyd_o" 
+                        title='states-game-demo' frameborder="0" 
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen='true'>
+                    </iframe>    
+                </div>
+           
+                <div>
+                    <iframe 
+                        className='media-object'
+                        width="560" height="315" src="https://www.youtube.com/embed/BNmb7fpG_LQ" 
+                        title='image-scrambler-game-demo' frameborder="0" 
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen='true'>
+                    </iframe>
+                </div>
 
-            <iframe 
-                width="560" height="315" src="https://www.youtube.com/embed/zBp52ufZFWQ" 
-                title='converter-demo' frameborder="0" 
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-                allowfullscreen='true'>
-            </iframe>
+            </div>
 
+            <div className='proj'>
+
+                <div>
+                    <iframe 
+                        className='media-object'
+                        width="560" height="315" src="https://www.youtube.com/embed/a4EL71uBcW8" 
+                        title='trip-tracker-demo' frameborder="0" 
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen='true'>
+                    </iframe>
+                </div>
+
+                <div>
+                    <iframe 
+                        className='media-object'
+                        width="560" height="315" src="https://www.youtube.com/embed/zBp52ufZFWQ" 
+                        title='converter-demo' frameborder="0" 
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen='true'>
+                    </iframe>
+                </div>
+
+            </div>
 
         </div>
     )
