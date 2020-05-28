5d492917f5e70ad8269083c1353093206aef56fc
kennja05 <jacobkenny05@gmail.com>
2020-05-28 12:04:45 -0400

    embedded video demos on project page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import Home from './Home'
 import './App.css';
 import { Route, Switch } from 'react-router-dom'
+
+import Home from './Home'
+import Projects from './Projects'
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route exact path = '/projects' component={Projects} />
       </Switch>
     </div>
   );
---
+++ src/Projects.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Projects = () => {
+    return(
+        <div>
+            
+            <iframe 
+                width="560" height="315" src="https://www.youtube.com/embed/SgrQfCzyd_o" 
+                title='states-game-demo' frameborder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen='true'>
+            </iframe>
+
+            <iframe 
+                width="560" height="315" src="https://www.youtube.com/embed/BNmb7fpG_LQ" 
+                title='image-scrambler-game-demo' frameborder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen='true'>
+            </iframe>
+
+            <iframe 
+                width="560" height="315" src="https://www.youtube.com/embed/a4EL71uBcW8" 
+                title='trip-tracker-demo' frameborder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen='true'>
+            </iframe>
+
+            <iframe 
+                width="560" height="315" src="https://www.youtube.com/embed/zBp52ufZFWQ" 
+                title='converter-demo' frameborder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen='true'>
+            </iframe>
+
+
+        </div>
+    )
+}
+
+export default Projects
