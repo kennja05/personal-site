24d088b5f13cc5e9fb9ed3420686f1511631908e
kennja05 <jacobkenny05@gmail.com>
2020-06-02 11:38:14 -0400

    created and added gifs to image scrambler

--- src/individualProjects/ImageScrambler.js
+++ src/individualProjects/ImageScrambler.js
@@ -54,7 +54,19 @@ const ImageScrambler = () => {
                     <a href='https://github.com/kennja05/Image-Scrambler-Game' target='_blank' rel='noopener noreferrer'>Github</a>
                 </p>
             </div>
-                
+            <div>
+                <iframe title='startup' src="https://giphy.com/embed/UrPH02ZBQqXk0ZW9Qk" 
+                width="480" height="270" 
+                frameBorder="0" class="giphy-embed" allowFullScreen={false}></iframe>
+
+                <iframe title='gameplay' src="https://giphy.com/embed/ckThXih9Pi1Oyzyh6M" 
+                width="480" height="270" 
+                frameBorder="0" class="giphy-embed" allowFullScreen={false}></iframe>
+
+                <iframe title='end' src="https://giphy.com/embed/WqG4OWOPnen5GVE036" 
+                width="480" height="270" 
+                frameBorder="0" class="giphy-embed" allowFullScreen={false}></iframe>
+            </div>
 
         </div>
     )
