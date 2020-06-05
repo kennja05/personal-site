87e36233c1296641d5e4f889822ecc48a0fe364a
kennja05 <jacobkenny05@gmail.com>
2020-06-04 22:10:40 -0400

    updated noContent

--- src/NoContent.js
+++ src/NoContent.js
@@ -12,7 +12,7 @@ class NoContent extends React.Component {
                 <h1>PAGE NOT FOUND</h1>
                 <h3>The requested page does not exist</h3>
                 <h3>Please <a style={{color: '#ff69b4', textDecoration: 'underline'}} 
-                    href='mailto:jacobkenny05@gmail.com'>message</a> Jacob if this is an error
+                    href='mailto:jacobkenny05@gmail.com'>send a message</a> if this is an error
                 </h3>
                 <Link className='icon' to='/'>
                     <FontAwesomeIcon aria-hidden='true' icon={faHome} size='2x' />
