963cd1a5bf97c764a43d066b64c31c225a8018fd
kennja05 <jacobkenny05@gmail.com>
2020-06-04 23:05:01 -0400

    updated about section

--- src/About.js
+++ src/About.js
@@ -16,31 +16,33 @@ class About extends React.Component {
                 <img id='profile-picture' alt='me' src={headshot} />
                 <div className='bio'>
                     <p>
-                        At the age of 26 I left what could end up to be the most stable employment 
-                        experience I will ever have as a union-backed employee of the Commonwealth of 
-                        Pennsylvania to try to learn how to code. After two years of toiling away in 
+                        At the age of 26 I left what could end up to be the most stable job 
+                        I will ever have as a union-backed employee of the Commonwealth of 
+                        Pennsylvania. After two years of toiling away in 
                         the basement of the State Treasury building I was ready for a change. When 
                         preparing to leave any position it's important to have a plan - which I think 
                         ideally would include a start date for a new position, however as I was 
                         preparing for my own departure I would frequently find that I was missing 
                         skills for the jobs that I wanted, and didn't have much interest in the jobs 
-                        for which I was qualified.
+                        for which I was qualified. I decided that I wanted to try to learn about 
+                        coding.
                     </p>
                     <p>
                         I had long held an interest in coding, but aside from completing a few random 
-                        tutorials here and there, I hadn’t really done much about it. However at that 
-                        big moment in my life I made a decision: instead of immediately seeking another 
+                        tutorials here and there, I hadn’t really done much of it. However at that 
+                        big moment in my life as I was getting ready to transition out of a role
+                        I made a decision: instead of immediately seeking another 
                         position, I elected to pursue that long-held interest in coding by completing 
                         an intensive program to learn computer programming.
                     </p>
                     <p>
                         So far, the thing that I have enjoyed the most is building websites using 
-                        React (such as this one). Sometimes things never make it past the localhost phase, but 
+                        React (such as this one). Sometimes things never make it past the "localhost" phase, but 
                         nonetheless, I’ve greatly enjoyed being able to take an idea, and quickly 
                         get it working in the browser.
                     </p>
                     <p>
-                        There’s a saying that goes “The best time to plant a tree was twenty years ago. 
+                        There’s a saying that goes:  “The best time to plant a tree was twenty years ago. 
                         The second best time is now.” It’s a line that resonates with me as I think it 
                         relates to my journey as a coder. Although I wish that I could go back in time 
                         and tell my childhood self to start coding, I cannot. So I do the next best 
