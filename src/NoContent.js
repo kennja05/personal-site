776b3c6d57f4a2863e9ea6c1844c3c0deb94985e
kennja05 <jacobkenny05@gmail.com>
2020-07-11 09:18:10 -0400

    update no content

--- src/NoContent.js
+++ src/NoContent.js
@@ -8,16 +8,18 @@ class NoContent extends React.Component {
 
     render(){
         return(
-            <div className='no-content-page'>
-                <h1>PAGE NOT FOUND</h1>
-                <h3>The requested page does not exist</h3>
-                <h3>Please <a style={{color: '#ff69b4', textDecoration: 'underline'}} 
-                    href='mailto:jacobkenny05@gmail.com'>send a message</a> if this is an error
-                </h3>
-                <Link className='icon' to='/'>
-                    <FontAwesomeIcon aria-hidden='true' icon={faHome} size='2x' />
-                </Link>
-                <a href='/' className='sr-only'>Return to Homepage</a>
+            <div className='container-fluid' id='no-content-page'>
+                <div id='no-content'>
+                    <h1>PAGE NOT FOUND</h1>
+                    <h3>The requested page does not exist</h3>
+                    {/* <h3>Please <a style={{color: '#ff69b4', textDecoration: 'underline'}} 
+                        href='mailto:jacobkenny05@gmail.com'>send a message</a> if this is an error
+                    </h3> */}
+                    <Link className='icon' to='/'>
+                        <FontAwesomeIcon aria-hidden='true' icon={faHome} size='2x' />
+                    </Link>
+                    <a href='/' className='sr-only'>Return to Homepage</a>
+                </div>
             </div>
         )
     }
